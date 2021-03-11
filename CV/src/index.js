9fcef59df188bfbc23524cb05718813167390e55
Will Dixon <will.dixon1@gmail.com>
2021-03-11 11:41:53 +0000

    unstaged changes to commit

--- CV/src/index.js
+++ CV/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import MenuContainer from './MenuContainer'; 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> 
+    <App />
+    <MenuContainer />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.querySelector("#container")
 );
 <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
 reportWebVitals();
